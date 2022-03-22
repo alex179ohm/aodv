e01184f3a659fb8e7b9595ab8343185f5227c3a3
alex179ohm <alex179ohm@gmail.com>
2022-03-22 22:58:50 +0100

    Add SIZE PACKETS constants

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,10 @@ use socket2::{Protocol, Type};
 pub const PORT: u32 = 654;
 pub const TYPE: Type = Type::DGRAM;
 pub const PROTOCOL: Protocol = Protocol::UDP;
+
+pub const RREQ_SIZE: usize = 192;
+pub const RREP_SIZE: usize = 160;
+pub const RERR_SIZE: usize = 160;
+pub const RREP_ACK_SIZE: usize = 16;
+
+pub const ACTIVE_ROUTE_TIMEOUT: usize = 0;
