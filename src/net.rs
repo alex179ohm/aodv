3f2ecd2aebef98030781f08877c6fda6b667298c
alex179ohm <alex179ohm@gmail.com>
2022-03-22 23:15:16 +0100

    Add net module

--- src/net.rs
+++ src/net.rs
@@ -1,6 +1,12 @@
-use std::{io, net::SocketAddr, time::Duration};
+#![allow(dead_code)]
 
-use socket2::{Domain, Socket};
+use std::{
+    io,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    time::Duration,
+};
+
+use socket2::{Domain, SockAddr, Socket};
 
 pub fn new_socket(addr: &SocketAddr) -> io::Result<Socket> {
     let domain = if addr.is_ipv4() {
@@ -13,5 +19,24 @@ pub fn new_socket(addr: &SocketAddr) -> io::Result<Socket> {
 
     sock.set_read_timeout(Some(Duration::from_millis(100)))?;
 
+    Ok(sock)
+}
+
+pub fn join_multicast(addr: SocketAddr) -> io::Result<Socket> {
+    let ip_addr = addr.ip();
+
+    let sock = new_socket(&addr)?;
+
+    match ip_addr {
+        IpAddr::V4(ref mdns_v4) => {
+            sock.join_multicast_v4(mdns_v4, &Ipv4Addr::new(0, 0, 0, 0))?;
+        }
+        IpAddr::V6(ref mdns_v6) => {
+            sock.join_multicast_v6(mdns_v6, 0)?;
+            sock.set_only_v6(true)?;
+        }
+    };
+
+    sock.bind(&SockAddr::from(addr))?;
     Ok(sock)
 }
